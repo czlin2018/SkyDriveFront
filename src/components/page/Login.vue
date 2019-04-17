7a722b03041fcd8acb1e595d4376bf668be71a01
czlin <953564227@qq.com>
2019-04-18 02:28:40 +0800

    修改登陆界面

--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -1,110 +1,121 @@
 <template>
-    <div class="login-wrap">
-        <div class="ms-login">
-            <div class="ms-title">陈泽林的分布式网盘管理系统</div>
-            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="ms-content">
-                <el-form-item prop="name">
-                    <el-input v-model="ruleForm.name" placeholder="账户(邮箱或用户名)">
-                        <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
-                    </el-input>
-                </el-form-item>
-                <el-form-item prop="password">
-                    <el-input type="password" placeholder="密码" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')">
-                        <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
-                    </el-input>
-                </el-form-item>
-                <div class="login-btn">
-                    <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-                </div>
-                <p class="login-tips">Tips : 缺少账号的用请注册</p>
-            </el-form>
+  <div class="login-wrap">
+    <div class="ms-login">
+      <div class="ms-title">陈泽林的分布式网盘管理系统</div>
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="ms-content">
+        <el-form-item prop="name">
+          <el-input v-model="ruleForm.name" placeholder="账户(邮箱或用户名)">
+            <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            placeholder="密码"
+            v-model="ruleForm.password"
+            @keyup.enter.native="submitForm('ruleForm')"
+          >
+            <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
+          </el-input>
+        </el-form-item>
+        <div class="login-btn">
+          <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
         </div>
+          <div class="login-btn">
+          <el-button type="success" @click="submitForm('ruleForm')">注册</el-button>
+        </div>
+        <p class="login-tips">Tips : 缺少账号的用请注册</p>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data: function(){
-            return {
-                ruleForm: {
-                    name: '',
-                    password: ''
-                },
-                rules: {
-                    name: [
-                        { required: true, message: '请输入用户名', trigger: 'blur' }
-                    ],
-                    password: [
-                        { required: true, message: '请输入密码', trigger: 'blur' }
-                    ]
-                }
-            }
-        },
-        methods: {
-            submitForm(formName) {
-                this.$refs[formName].validate((valid) => {
-                    if (valid) {
-                        this.$axios.post('http://localhost:8082/register/joinIn', this.ruleForm).then((res) => {
-                           console.log('res', res);
-                           if(res.data.code === 0) {
-                            localStorage.setItem('ms_username',this.ruleForm.name);
-                            this.$router.push('/');
-                           }
-                           else {
-                               this.$message.error(res.data.msg);
-                           }
-
-                        });
-                    } else {
-                        console.log('error submit!!');
-                        return false;
-                    }
+export default {
+  data: function() {
+    return {
+      ruleForm: {
+        name: "",
+        password: ""
+      },
+      rules: {
+        name: [{ required: true, message: "请输入用户名", trigger: "blur" }],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.$axios
+            .post("http://localhost:9100/register/joinIn", this.ruleForm)
+            .then(res => {
+              console.log("res", res);
+              if (res.data.code === 0) {
+                this.$message.success({
+                  message: res.data.msg,
+                  center: true
                 });
-            }
+                localStorage.setItem("ms_username", res.data.data);
+                this.$router.push("/");
+              } else {
+                this.$message.error({
+                  message: res.data.msg,
+                  center: true
+                });
+              }
+            });
+        } else {
+          console.log("error submit!!");
+          return false;
         }
+      });
     }
+  }
+};
 </script>
 
 <style scoped>
-    .login-wrap{
-        position: relative;
-        width:100%;
-        height:100%;
-        background-image: url(../../assets/img/login-bg.jpg);
-        background-size: 100%;
-    }
-    .ms-title{
-        width:100%;
-        line-height: 50px;
-        text-align: center;
-        font-size:20px;
-        color:black;
-        border-bottom: 1px solid #ddd;
-    }
-    .ms-login{
-        position: absolute;
-        left:50%;
-        top:50%;
-        width:350px;
-        margin:-190px 0 0 -175px;
-        border-radius: 5px;
-        background: rgba(255,255,255, 0.3);
-        overflow: hidden;
-    }
-    .ms-content{
-        padding: 30px 30px;
-    }
-    .login-btn{
-        text-align: center;
-    }
-    .login-btn button{
-        width:100%;
-        height:36px;
-        margin-bottom: 10px;
-    }
-    .login-tips{
-        font-size:12px;
-        line-height:30px;
-        color:#fff;
-    }
+.login-wrap {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-image: url(../../assets/img/login-bg.jpg);
+  background-size: 100%;
+}
+.ms-title {
+  width: 100%;
+  line-height: 50px;
+  text-align: center;
+  font-size: 20px;
+  color: black;
+  border-bottom: 1px solid #ddd;
+}
+.ms-login {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  width: 350px;
+  margin: -190px 0 0 -175px;
+  border-radius: 5px;
+  background: rgba(255, 255, 255, 0.3);
+  overflow: hidden;
+}
+.ms-content {
+  padding: 30px 30px;
+}
+.login-btn {
+  text-align: center;
+}
+.login-btn button {
+  width: 100%;
+  height: 36px;
+  margin-bottom: 10px;
+}
+.login-tips {
+  font-size: 15px;
+  line-height: 30px;
+  color: #fff;
+}
 </style>
