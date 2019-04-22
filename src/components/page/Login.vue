34f2667c14848292684576847f04c8a1b5ff2a7c
czlin <953564227@qq.com>
2019-04-23 00:56:41 +0800

    增加美化插件,修改确定密码提示

--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -3,96 +3,111 @@
     <div class="ms-login">
       <div class="ms-title">陈泽林的分布式网盘管理系统</div>
       <div v-if="!showReg">
-      <el-form  :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="ms-content"> 
-        <el-form-item prop="name" >
-          <el-input v-model="ruleForm.name" placeholder="账户(邮箱或用户名)">
-            <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
-          </el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input
-            type="password"
-            placeholder="密码"
-            v-model="ruleForm.password"
-            @keyup.enter.native="submitForm(ruleForm)"
-          >
-            <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
-          </el-input>
-        </el-form-item>
-        <div class="login-btn">
-          <el-button type="primary" @click="submitForm(ruleForm)">登录</el-button>
-        </div>
-        <p class="login-tips">Tips : 缺少账号的用请
-          <el-button type="text" size="medium" @click="openReg">注册</el-button>
-        </p>
-      </el-form>
-      </div>
-       <div v-if="showReg">
-      <el-form v-if="showReg" :model="regForm" :rules="regRules" ref="regForm" label-width="0px" class="ms-content"> 
-        <el-form-item prop="names" >
-          <el-input v-model="regForm.names" placeholder="请输入用户名">
-            <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
-          </el-input>
-          </el-form-item>
-          <el-form-item prop="email" >
-            <el-input v-model="regForm.email" placeholder="请输入邮箱地址">
-               <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
+        <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="0px"
+          class="ms-content"
+        >
+          <el-form-item prop="name">
+            <el-input v-model="ruleForm.name" placeholder="账户(邮箱或用户名)">
+              <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
             </el-input>
-        </el-form-item>
-        <el-form-item prop="passwords">
-          <el-input
-            type="password"
-            placeholder="请输入密码"
-            v-model="regForm.passwords"
-          >
-            <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
-          </el-input>
-        </el-form-item>
-        <el-form-item prop="checkPass">
-          <el-input
-            type="password"
-            placeholder="请再次输入密码"
-            v-model="regForm.checkPass"
-          >
-            <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
-          </el-input>
-        </el-form-item>
-
-        <el-form-item prop="code">
-          <el-input
-            placeholder="请输入邮箱验证码"
-            v-model="regForm.code"
-            style="width:60%;margin-right:10px"
-          >
-            <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
-          </el-input>
-           <el-button type="primary" size="mini" @click="acceptCode" :disabled="disabled">{{codeStauts}}</el-button>
-        </el-form-item>
-           <el-form-item>
-                <el-radio disabled v-model="regForm.userType" label="admin" >管理员</el-radio>
-                <el-radio checked v-model="regForm.userType" label="user">普通用户</el-radio>
           </el-form-item>
-        <div class="login-btn">
-          <el-button type="primary" @click="reg(regForm)">注册</el-button>
-        </div>
-      </el-form>
-       </div>
+          <el-form-item prop="password">
+            <el-input
+              type="password"
+              placeholder="密码"
+              v-model="ruleForm.password"
+              @keyup.enter.native="submitForm(ruleForm)"
+            >
+              <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
+            </el-input>
+          </el-form-item>
+          <div class="login-btn">
+            <el-button type="primary" @click="submitForm(ruleForm)">登录</el-button>
+          </div>
+          <p class="login-tips">
+            Tips : 缺少账号的用请
+            <el-button type="text" size="medium" @click="openReg">注册</el-button>
+          </p>
+        </el-form>
+      </div>
+      <div v-if="showReg">
+        <el-form
+          v-if="showReg"
+          :model="regForm"
+          :rules="regRules"
+          ref="regForm"
+          label-width="0px"
+          class="ms-content"
+        >
+          <el-form-item prop="names">
+            <el-input v-model="regForm.names" placeholder="请输入用户名">
+              <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="email">
+            <el-input v-model="regForm.email" placeholder="请输入邮箱地址">
+              <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="passwords">
+            <el-input type="password" placeholder="请输入密码" v-model="regForm.passwords">
+              <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="checkPass">
+            <el-input type="password" placeholder="请再次输入密码" v-model="regForm.checkPass">
+              <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
+            </el-input>
+          </el-form-item>
+
+          <el-form-item prop="code">
+            <el-input
+              placeholder="请输入邮箱验证码"
+              v-model="regForm.code"
+              style="width:60%;margin-right:10px"
+            >
+              <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
+            </el-input>
+            <el-button
+              type="primary"
+              size="mini"
+              @click="acceptCode"
+              :disabled="disabled"
+            >{{codeStauts}}</el-button>
+          </el-form-item>
+          <el-form-item>
+            <el-radio disabled v-model="regForm.userType" label="admin">管理员</el-radio>
+            <el-radio checked v-model="regForm.userType" label="user">普通用户</el-radio>
+          </el-form-item>
+          <div class="login-btn">
+            <el-button type="primary" @click="reg(regForm)">注册</el-button>
+          </div>
+        </el-form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { connect } from "net";
 export default {
   data() {
     var validatePass2 = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请再次输入密码'));
-        } else if (value !== this.ruleForm.passwords) {
-          callback(new Error('两次输入密码不一致!'));
-        } else {
-          callback();
-        }
-      };
+      if (value === "") {
+        console.log("value", value);
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.regForm.passwords) {
+        console.log("value", value);
+        console.log("this.regForm.passwords", this.regForm.passwords);
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
     return {
       ruleForm: {
         name: "",
@@ -105,98 +120,102 @@ export default {
       regForm: {
         names: "",
         passwords: "",
-        checkPass:'',
-        email:'',
-        code:'',
-        userType:'user'
+        checkPass: "",
+        email: "",
+        code: "",
+        userType: "user"
       },
       regRules: {
         names: [{ required: true, message: "请输入用户名", trigger: "blur" }],
         passwords: [{ required: true, message: "请输入密码", trigger: "blur" }],
-        checkPass: [
-            { validator: validatePass2, trigger: 'blur' }
-          ],
-        email:[
-            { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-            { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
-              ],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+        email: [
+          { required: true, message: "请输入邮箱地址", trigger: "blur" },
+          {
+            type: "email",
+            message: "请输入正确的邮箱地址",
+            trigger: ["blur", "change"]
+          }
+        ],
         code: [{ required: true, message: "请输入验证码", trigger: "blur" }]
       },
-      showReg:false, //控制注册页面
-      disabled:false,
-      codeStauts:'获取验证码'
+      showReg: false, //控制注册页面
+      disabled: false,
+      codeStauts: "获取验证码"
     };
   },
   methods: {
     submitForm(formName) {
-      const par ={
-            name:formName.name,
-            password:formName.password
+      const par = {
+        name: formName.name,
+        password: formName.password
+      };
+      this.$axios
+        .post("http://localhost:9100/register/joinIn", par)
+        .then(res => {
+          console.log("res", res);
+          if (res.data.code === 0) {
+            this.$message.success({
+              message: res.data.msg,
+              center: true
+            });
+            localStorage.setItem("ms_username", res.data.data);
+            this.$router.push("/");
+          } else {
+            this.$message.error({
+              message: res.data.msg,
+              center: true
+            });
           }
-          this.$axios
-            .post("http://localhost:9100/register/joinIn", par)
-            .then(res => {
-              console.log("res", res);
-              if (res.data.code === 0) {
-                this.$message.success({
-                  message: res.data.msg,
-                  center: true
-                });
-                localStorage.setItem("ms_username", res.data.data);
-                this.$router.push("/");
-              } else {
-                this.$message.error({
-                  message: res.data.msg,
-                  center: true
-                });
-              }
+        });
+    },
+    openReg() {
+      this.showReg = true;
+    },
+    reg(regName) {
+      const par = {
+        verificationCode: regName.code,
+        userId: regName.email,
+        password: regName.passwords,
+        name: regName.names,
+        userType: regName.userType
+      };
+      this.$axios
+        .post("http://localhost:9100/register/insert", par)
+        .then(res => {
+          console.log("res", res);
+          if (res.data.code === 0) {
+            this.$message.success({
+              message: res.data.msg,
+              center: true
             });
-    },
-    openReg(){
-      this.showReg = true
-    },
-    reg(regName){
-      
-    const par ={
-            verificationCode:regName.code,
-            userId:regName.email,
-            password:regName.passwords,
-            name:regName.names,
-            userType:regName.userType
+            //localStorage.setItem("ms_username", res.data.data);
+            this.showReg = false;
+            this.ruleForm.name = regName.email;
+            this.ruleForm.password = regName.passwords;
+          } else {
+            this.$message.error({
+              message: res.data.msg,
+              center: true
+            });
           }
-          this.$axios
-            .post("http://localhost:9100/register/insert", par)
-            .then(res => {
-              console.log("res", res);
-              if (res.data.code === 0) {
-                this.$message.success({
-                  message: res.data.msg,
-                  center: true
-                });
-                //localStorage.setItem("ms_username", res.data.data);
-                this.showReg = false
-                this.ruleForm.name = regName.email
-                this.ruleForm.password = regName.passwords
-              } else {
-                this.$message.error({
-                  message: res.data.msg,
-                  center: true
-                });
-              }
-            });
+        });
     },
-    acceptCode(){
-       this.$axios
-            .get("http://localhost:9100/register/getVerificationCode?userId=" + this.regForm.email)
-            .then(res => {
-              if (res.data.code === 0) {
-                this.disabled = true
-                this.codeStauts = '获取成功'
-              } else {
-                this.disabled = false
-                this.codeStauts = '重新获取'
-              }
-            });
+    acceptCode() {
+      this.$axios
+        .get(
+          "http://localhost:9100/register/getVerificationCode?userId=" +
+            this.regForm.email
+        )
+        .then(res => {
+          if (res.data.code === 0) {
+            this.disabled = true;
+            this.codeStauts = "获取成功";
+          } else {
+            this.disabled = false;
+            this.codeStauts = "重新获取";
+          }
+        });
     }
   }
 };
