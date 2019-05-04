581d52b3d9320b12a42ad86a84f3a721672b5474
czlin <953564227@qq.com>
2019-05-04 17:22:24 +0800

    完成上传下载

---
+++ src/components/page/UPloadAddDownload.vue
@@ -0,0 +1,315 @@
+<template>
+  <div style="margin:15px;">
+    <div style="margin-bottom:15px;">
+      <el-button type="primary" size="mini" @click="openFile">
+        创建文件夹
+        <i class="el-icon-edit el-icon--right"></i>
+      </el-button>
+      <el-upload
+        class="upload-demo"
+        :data="uploadData"
+        :show-file-list="false"
+        action="http://localhost:9200/createFile"
+        :on-success="upload_success"
+        :limit="1"
+        style="display:inline-block;margin: 0 5px;"
+      >
+        <el-button type="primary" size="mini" @click="upload">
+          上传
+          <i class="el-icon-upload el-icon--right"></i>
+        </el-button>
+      </el-upload>
+    </div>
+    <div style="margin-bottom:15px;">
+      <el-button type="text" size="mini" :disabled="showBtn" @click="backPrev">返回上一级</el-button>
+      <span style="display:inline-block;margin: 0 5px;">|</span>
+      <el-button type="text" size="mini" @click="backAll" :disabled="showBtn">全部文件</el-button>
+      <span v-if="this.path !== '/'" style="display:inline-block;margin: 0 5px;">- {{showPath}}</span>
+    </div>
+    <el-table :data="tableData" :height="height" style="width: 100%">
+      <el-table-column label="文件夹名称">
+        <template slot-scope="scope">
+          <el-button
+            v-if="scope.row.directory"
+            type="text"
+            size="mini"
+            @click="changePath(scope.row.name)"
+          >{{scope.row.name}}</el-button>
+          <span v-if="!scope.row.directory">{{scope.row.name}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="120px" align="center">
+        <template slot-scope="scope">
+          <el-button
+            v-if="!scope.row.directory"
+            type="text"
+            size="mini"
+            @click="downLoad(scope.row.name)"
+          >下载</el-button>
+          <span v-if="!scope.row.directory" style="display:inline-block;margin: 0 5px;">|</span>
+          <el-button
+            v-if="!scope.row.directory"
+            type="text"
+            size="mini"
+            @click="deletefile(scope.row)"
+          >删除</el-button>
+          <el-button
+            v-if="scope.row.directory"
+            type="text"
+            size="mini"
+            @click="deletefile(scope.row)"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="pagination">
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
+    </div>
+    <el-dialog title="创建文件夹" :visible.sync="fileVisible" width="450px" top="30vh">
+      <el-input
+        size="mini"
+        v-model="fileName"
+        style="display:inline-block;width:340px;margin-right:10px;"
+      ></el-input>
+      <el-button type="primary" size="mini" @click="saveFile">保存</el-button>
+    </el-dialog>
+  </div>
+</template>
+<script>
+import { constants } from "crypto";
+export default {
+  name: "uPloadAddDownload",
+  data() {
+    return {
+      currentPage: 1,
+      pageSize: 10,
+      total: 0,
+      height: document.documentElement.clientHeight - 295,
+      tableData: [],
+      path: "/", //请求路径
+      fileVisible: false, //创建文件夹弹窗
+      fileName: "", //新建文件名
+      curruntFile: "", //当前选择文件夹
+      fileList: [],
+      uploadData: {},
+      delPath: ""
+    };
+  },
+  computed: {
+    showBtn() {
+      if (this.path !== "/") {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    showPath() {
+      return this.path.slice(1);
+    }
+  },
+  created() {
+    this.funData();
+  },
+  methods: {
+    // 分页导航
+    handleSizeChange(val) {
+      this.pageSize = val;
+      const url =
+        "http://localhost:9200/readPathInfo?path=" +
+        this.path +
+        "&pageNo=" +
+        this.currentPage +
+        "&pageSize=" +
+        val;
+      this.$axios.get(url).then(res => {
+        if (res.data.code === 0) {
+          this.$message.success({
+            message: res.data.msg,
+            center: true
+          });
+          this.tableData = res.data.data.pageData;
+          this.total = res.data.data.totalCount;
+        } else {
+          this.$message.error({
+            message: res.data.msg,
+            center: true
+          });
+        }
+      });
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val;
+      const url =
+        "http://localhost:9200/readPathInfo?path=" +
+        this.path +
+        "&pageNo=" +
+        val +
+        "&pageSize=" +
+        this.pageSize;
+      this.$axios.get(url).then(res => {
+        if (res.data.code === 0) {
+          this.$message.success({
+            message: res.data.msg,
+            center: true
+          });
+          this.tableData = res.data.data.pageData;
+          this.total = res.data.data.totalCount;
+        } else {
+          this.$message.error({
+            message: res.data.msg,
+            center: true
+          });
+        }
+      });
+    },
+    funData() {
+      const url =
+        "http://localhost:9200/readPathInfo?path=" +
+        this.path +
+        "&pageNo=" +
+        "1" +
+        "&pageSize=" +
+        "10";
+      this.$axios.get(url).then(res => {
+        if (res.data.code === 0) {
+          this.$message.success({
+            message: res.data.msg,
+            center: true
+          });
+          this.tableData = res.data.data.pageData;
+          this.total = res.data.data.totalCount;
+        } else {
+          this.$message.error({
+            message: res.data.msg,
+            center: true
+          });
+        }
+      });
+    },
+    // 进入文件夹获取新文件数组
+    changePath(name) {
+      this.curruntFile = name;
+      this.path = this.path === "/" ? this.path + name : this.path + "/" + name;
+      this.funData();
+    },
+    // 打开创建文件夹弹窗
+    openFile() {
+      this.fileVisible = true;
+    },
+    // 保存新增文件夹
+    saveFile() {
+      const par = {
+        path:
+          this.path === "/"
+            ? this.path + this.fileName
+            : this.path + "/" + this.fileName
+      };
+
+      this.$axios.post("http://localhost:9200/mkdir", par).then(res => {
+        if (res.data.code === 0) {
+          this.$message.success({
+            message: res.data.msg,
+            center: true
+          });
+          this.funData();
+          this.fileVisible = false;
+          this.fileName = "";
+        } else {
+          this.$message.error({
+            message: res.data.msg,
+            center: true
+          });
+        }
+      });
+    },
+    // 返回全部文件
+    backAll() {
+      this.path = "/";
+      this.funData();
+    },
+    // 返回上一级
+    backPrev() {
+      const path = this.path.split("/");
+      const length = path[path.length - 1].length;
+      const end = this.path.length - length - 1;
+      this.path = this.path.slice(0, end);
+      if (this.path === "") {
+        this.path = "/";
+      }
+      console.log(this.path);
+      this.funData();
+    },
+    upload() {
+      const par = {
+        path: this.path
+      };
+      this.uploadData.path = par.path;
+    },
+    upload_success(response, file, fileList) {
+      this.funData();
+    },
+    downLoad(name) {
+      const par = {
+        path: this.path === "/" ? this.path + name : this.path + "/" + name
+      };
+      this.$axios.post("http://localhost:9200/downloadFile", par).then(res => {
+        if (res.data.code === 0) {
+          this.$message.success({
+            message: res.data.msg,
+            center: true
+          });
+        } else {
+          this.$message.error({
+            message: res.data.msg,
+            center: true
+          });
+        }
+      });
+    },
+    deletefile(row) {
+      this.delPath =
+        this.path === "/" ? this.path + row.name : this.path + "/" + row.name;
+      console.log(this.delPath);
+      this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$axios
+            .get("http://localhost:9200/deleteFile?path=" + this.delPath)
+            .then(res => {
+              if (res.data.code === 0) {
+                this.$message.success({
+                  message: res.data.msg,
+                  center: true
+                });
+                this.funData();
+              } else {
+                this.$message.error({
+                  message: res.data.msg,
+                  center: true
+                });
+              }
+            });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    }
+  }
+};
+</script>
+
